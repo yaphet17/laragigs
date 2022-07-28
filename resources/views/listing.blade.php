92385bde372c13830f9d3cda6875c5980bf27071
yaphet17 <yafetberhanu3@gmail.com>
2022-07-28 13:33:11 +0300

    Add components

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -7,7 +7,7 @@
     ><i class="fa-solid fa-arrow-left"></i> Back
     </a>
     <div class="mx-4">
-        <div class="bg-gray-50 border border-gray-200 p-10 rounded">
+        <x-card>
             <div
                 class="flex flex-col items-center justify-center text-center"
             >
@@ -67,7 +67,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </x-card>
     </div>
 @else
     <p>Invalid id</p>
